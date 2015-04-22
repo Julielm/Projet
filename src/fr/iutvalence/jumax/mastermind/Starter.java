869e1bc3754ffb2556446454b70cdc52220a6284
Anthony Gelibert <anthony.gelibert@me.com>
2015-04-23 01:06:08 +0200

    Starter.java: simplify array creation

--- src/fr/iutvalence/jumax/mastermind/Starter.java
+++ src/fr/iutvalence/jumax/mastermind/Starter.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package fr.iutvalence.jumax.mastermind;
 
 /**
@@ -15,11 +12,7 @@ public class Starter
 	public static void main(String[] args)
 	{
 		Player player = new Player("Player1");
-		Color[] secret = new Color[Grid.COLUMNS_NB];
-		secret[0] = Color.GREEN;
-		secret[1] = Color.BLACK;
-		secret[2] = Color.PURPLE;
-		secret[3] = Color.YELLOW;
+		Color[] secret = {Color.GREEN,  Color.BLACK, Color.PURPLE, Color.YELLOW};
 		// Display of secret's colors.
 		for(int columnNumber=0; columnNumber<Grid.COLUMNS_NB;columnNumber++)
 			System.out.println(secret[columnNumber]);
