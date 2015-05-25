99fd64bdc68f07140f233bbed1ba0f3a4dbffb4e
Julielm <julielm.leclerc@hotmail.fr>
2015-05-25 16:56:36 +0200

    Improvements of the navigation window.

--- src/fr/iutvalence/jumax/mastermind/view/gui/Navigation.java
+++ src/fr/iutvalence/jumax/mastermind/view/gui/Navigation.java
@@ -1,15 +1,19 @@
 package fr.iutvalence.jumax.mastermind.view.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 
 public class Navigation extends JFrame
 {
-	private JPanel container= new JPanel();
 
 	public Navigation() {
 		this.setTitle("Mastermind");
@@ -17,19 +21,31 @@ public class Navigation extends JFrame
 	    this.setResizable(false);
 	    this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-	    
-	    container.setLayout(new BorderLayout());
-
+	    	    
 	    JLabel title=new JLabel("Mastermind");
-		JPanel menu= new JPanel();
-	    container.add(title, BorderLayout.NORTH);
-	    container.add(menu, BorderLayout.CENTER);
+	    title.setHorizontalAlignment(SwingConstants.CENTER);
+	    title.setFont(new Font("MV Boli", Font.PLAIN, 45));
+	    this.getContentPane().add(title, BorderLayout.NORTH);
 	    
-	    JButton play = new JButton("Play");
-	    JButton scores = new JButton("Scores");
-	    menu.add(play);
-	    menu.add(scores);
-	    this.getContentPane().add(container);
+		JPanel menu= new JPanel();
+		menu.setBorder(UIManager.getBorder("Button.border"));
+		this.getContentPane().add(menu, BorderLayout.CENTER);
+		menu.setLayout(null);
+		
+		JButton play = new JButton("PLAY");
+		play.setBounds(66, 38, 364, 155);
+		play.setFont(new Font("Shruti", Font.PLAIN, 24));
+		
+		menu.add(play);
+		
+		JButton scores = new JButton("SCORES");
+		scores.setBounds(66, 213, 364, 155);
+		scores.setFont(new Font("Shruti", Font.PLAIN, 24));
+		
+		menu.add(scores);
+	
 	    this.setVisible(true);
+	    
+	   
 	}
 }
